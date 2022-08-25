368c21cb3baac17cf6501530d45fc010814d999c
sneharm1997 <sneharm1997@gmail.com>
2022-08-25 12:59:57 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-9wz3ez
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-9wz3ez)
---
+++ projects/dash-btwn.js
@@ -0,0 +1,13 @@
+let str = '36478855';
+let result = [str[0]];
+
+for (let x = 1; x < str.length; x++) {
+  if (str[x - 1] % 2 === 0 && str[x] % 2 === 0) {
+    result.push('-', str[x]);
+  } else {
+    result.push(str[x]);
+  }
+}
+console.log(result.join(''));
+
+//Output : 36-478-855
